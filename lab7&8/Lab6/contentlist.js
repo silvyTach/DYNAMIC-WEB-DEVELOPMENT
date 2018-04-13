a242bbc36d604ad7e796569dd0f2708520a9a83f
AshayaPi <silviya_tacheva@outlook.com>
2018-04-13 19:28:29 +0100

    creating new elements rather than changing innerHTML

--- lab7&8/Lab6/contentlist.js
+++ lab7&8/Lab6/contentlist.js
@@ -4,12 +4,23 @@ function addContent () {
 	var items = ["hewey", "dewey", "louie"];
 
 	// build the html string for a <ul> list
-	var items_html = "<ul>";
-	for (var i=0; i < items.length; i++) {
-		item = items[i];
-		items_html += "<li>" + item + "</li>";
-	};
-	items_html += "</ul>";
+	var list = document.createElement("ul");
+	document.body.appendChild(list);
+
+	//list.appendChild(document.createTextNode("ndhkfjsg"));
+
+	for (var i = 0; i < items.length; i++) {
+		var new_item = document.createElement("li");
+		var text = document.createTextNode(items[i]);
+		new_item.appendChild(text);
+		list.appendChild(new_item);
+	}
+	// var items_html = "<ul>";
+	// for (var i=0; i < items.length; i++) {
+	// 	item = items[i];
+	// 	items_html += "<li>" + item + "</li>";
+	// };
+	// items_html += "</ul>";
 
 	// using javascript
 	// 1. find the content div
