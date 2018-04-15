e686c6dd565778e9288a8a8345dabf05d1a5ceb1
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-15 02:10:55 +0100

    Making sure it's seeing the public folder.

--- Coursework/server.js
+++ Coursework/server.js
@@ -5,6 +5,9 @@ var app = express();
 app.set('view engine', 'ejs');
 //Setting the view engine to ejs
 
+app.use(express.static('/public/'));
+//Looking in the public folder for static files
+
 //Using res.render to load up an ejs view file:
 
 app.get('/', function(req, res) {
