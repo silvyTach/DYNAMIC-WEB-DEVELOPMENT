dcd2c5d065da6161b79b818df5cf8d9faf3569c9
AshayaPi <silviya_tacheva@outlook.com>
2018-04-15 13:14:54 +0100

    img tag

--- lab10/mars-example/roverparser.js
+++ lab10/mars-example/roverparser.js
@@ -20,12 +20,12 @@
   console.log(result.photos[0]);
   var photos = result.photos //this data contains an array called photos
 
-  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th><th>Date Taken</th></tr></thead><tbody>";
+  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image</th><th>Date Taken</th></tr></thead><tbody>";
   for (var i in photos)
   {
     output+="<tr><td>" + photos[i].rover.name + "</td><td>"
     + photos[i].camera.full_name + "</td><td>"
-    + photos[i].img_src + "</td><td>"
+    + "<img src=&ldquo;" + photos[i].img_src + "&rdquo;></td><td>"
     + photos[i].rover.launch_date + "</td></tr>";
   }
   output+="</tbody></table>";
