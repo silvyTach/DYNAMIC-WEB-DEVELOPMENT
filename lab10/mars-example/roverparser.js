363345358cbe8d214e2eba38a5d23cc67ecdef2a
AshayaPi <silviya_tacheva@outlook.com>
2018-04-15 12:42:15 +0100

    tr tag

--- lab10/mars-example/roverparser.js
+++ lab10/mars-example/roverparser.js
@@ -20,7 +20,7 @@
   console.log(result.photos);
   var photos = result.photos //this data contains an array called photos
 
-  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th><th>Date Taken</th></thead><tbody>";
+  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th><th>Date Taken</th></tr></thead><tbody>";
   for (var i in photos)
   {
     output+="<tr><td>" + photos[i].rover.name + "</td><td>"
