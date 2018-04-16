c062b8cf4c8bc28139fd55d9fcc6f4ef1a7adaa8
Silviya Tacheva <s.m.tacheva@rgu.ac.uk>
2018-04-16 15:08:33 +0000

    Knock knock jokes

---
+++ lab13/mythirdserver.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var knockknock = require('knock-knock-jokes');
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': ‘text/html'});
+var randomJoke = knockknock()
+ res.end(randomJoke);
+}).listen(8080);
