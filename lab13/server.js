e13148e5be2c2479f8db1b6dfbaa9db176535146
Silviya Tacheva <s.m.tacheva@rgu.ac.uk>
2018-04-16 17:14:08 +0000

    fixed error

--- lab13/server.js
+++ lab13/server.js
@@ -16,7 +16,7 @@ app.get('/joke', function(req, res){
 app.listen(8080);
 
 app.get('/add', function(req, res){
-var x = req.query.x;
-var y = req.query.y;
- res.send("X + Y="+parseInt(x+y));
+var x = parseInt(req.query.x);
+var y = parseInt(req.query.y);
+ res.send("X + Y="+(x+y));
 });
