d1342c9064273db480a417189a50ce136574fc41
Silviya Tacheva <s.m.tacheva@rgu.ac.uk>
2018-04-16 18:14:02 +0000

    Adding getform

--- lab13/server.js
+++ lab13/server.js
@@ -45,3 +45,9 @@ if(div == "") {
 });
 
 app.use(express.static('public'));
+
+app.get('/getform', function(req, res){
+var name = req.query.name;
+var quest = req.query.quest;
+ res.send("Hi "+name+" I am sure you will "+quest) ;
+});
