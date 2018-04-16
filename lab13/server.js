b1f0df18ba3a1ab8f6e90e5baba06efa606413c4
Silviya Tacheva <s.m.tacheva@rgu.ac.uk>
2018-04-16 17:02:06 +0000

    Adding route 2

--- lab13/server.js
+++ lab13/server.js
@@ -3,4 +3,9 @@ var app = express();
 app.get('/', function(req, res){
  res.send("Hello world! by express");
 });
+
+app.get('/test', function(req, res){
+ res.send("this is route 2");
+});
+
 app.listen(8080);
