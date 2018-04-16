3cfdb0cb9f933bdc4f60744ad26bd58facd46f14
Silviya Tacheva <s.m.tacheva@rgu.ac.uk>
2018-04-16 17:06:34 +0000

    Jokes Route

--- lab13/server.js
+++ lab13/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var knockknock = require('knock-knock-jokes');
 var app = express();
 app.get('/', function(req, res){
  res.send("Hello world! by express");
@@ -8,4 +9,8 @@ app.get('/test', function(req, res){
  res.send("this is route 2");
 });
 
+app.get('/joke', function(req, res){
+    var randomJoke = knockknock()
+    res.send(randomJoke);
+});
 app.listen(8080);
