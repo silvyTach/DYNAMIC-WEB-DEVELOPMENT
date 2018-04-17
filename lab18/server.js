e2703667da775dab6a2180cdd540fa0780849f8b
AshayaPi <silviya_tacheva@outlook.com>
2018-04-17 01:03:50 +0100

    uname

--- lab18/server.js
+++ lab18/server.js
@@ -114,7 +114,7 @@ app.post('/dologin', function(req, res) {
     //if there is no result, redirect the user back to the login system as that username must not exist
     if(!result){res.redirect('/login');return}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
-    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
+    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/', uname) }
     //otherwise send them back to login
     else{res.redirect('/login')}
   });
