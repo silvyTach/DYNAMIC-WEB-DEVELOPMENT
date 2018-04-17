693e34964872512b27aacc12be94ec7bf4e9a249
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-17 18:03:36 +0100

    Adding error message.

--- Coursework/public/scripts/searchresults.js
+++ Coursework/public/scripts/searchresults.js
@@ -31,32 +31,40 @@ function addResults(jsondata) {
   //Logging the JSON data to allow for data mining
   var htmlstring = "";
   //Creating a String to contain the HTML to inject
-  for (var i = 0; i < 10; i++) {
-    var title = jsondata.Search[i].Title;
-    var poster = jsondata.Search[i].Poster;
-    //Storing the parameters of each result in a variable
+  if (jsondata) {
+    for (var i = 0; i < 10; i++) {
+      var title = jsondata.Search[i].Title;
+      var poster = jsondata.Search[i].Poster;
+      //Storing the parameters of each result in a variable
 
-    htmlstring += '<div class="grid-100 grid-parent result">' + '\n';
-    htmlstring += '  <div class="grid-80">' + '\n';
-    htmlstring += '    <div class="text">' + '\n';
-    htmlstring += '      <h1 class="title">' + title + '</h1>' + '\n';
-    htmlstring += '      <ul class="info">' + '\n';
-    htmlstring += '        <li class="rating">⭐️ 7.4 <i> /10</i></li>' + '\n';
-    htmlstring += '        <li class="duration">Run Time: 2h 21m</li>' + '\n';
-    htmlstring += '        <li class="age">Rated: PG-13</li>' + '\n';
-    htmlstring += '      </ul>' + '\n';
-    htmlstring += '    </div>' + '\n';
-    htmlstring += '    <div class="moredetails">' + '\n';
-    htmlstring += '      <button class="button">More Details</button>' + '\n';
-    htmlstring += '    </div>' + '\n';
-    htmlstring += '  </div>' + '\n';
-    htmlstring += '\n';
-    htmlstring += '  <div class="grid-20">' + '\n';
-    htmlstring += '    <img class="poster" src="' + poster + '" alt = "Poster">' + '\n';
-    htmlstring += '  </div>' + '\n';
-    htmlstring += '</div>' + '\n';
-    htmlstring += '\n';
-  } //Iteratng over the collection of results
+      htmlstring += '<div class="grid-100 grid-parent result">' + '\n';
+      htmlstring += '  <div class="grid-80">' + '\n';
+      htmlstring += '    <div class="text">' + '\n';
+      htmlstring += '      <h1 class="title">' + title + '</h1>' + '\n';
+      htmlstring += '      <ul class="info">' + '\n';
+      htmlstring += '        <li class="rating">⭐️ 7.4 <i> /10</i></li>' + '\n';
+      htmlstring += '        <li class="duration">Run Time: 2h 21m</li>' + '\n';
+      htmlstring += '        <li class="age">Rated: PG-13</li>' + '\n';
+      htmlstring += '      </ul>' + '\n';
+      htmlstring += '    </div>' + '\n';
+      htmlstring += '    <div class="moredetails">' + '\n';
+      htmlstring += '      <button class="button">More Details</button>' + '\n';
+      htmlstring += '    </div>' + '\n';
+      htmlstring += '  </div>' + '\n';
+      htmlstring += '\n';
+      htmlstring += '  <div class="grid-20">' + '\n';
+      htmlstring += '    <img class="poster" src="' + poster + '" alt = "Poster">' + '\n';
+      htmlstring += '  </div>' + '\n';
+      htmlstring += '</div>' + '\n';
+      htmlstring += '\n';
+    } //Iteratng over the collection of results
+  } else {
+    htmlstring += '<div id="errormessage">\n';
+    htmlstring += '  <h1 class="title">Oops!</h1>\n';
+    htmlstring += "  <p>We couldn't find anything with that in the title. Please try again with a different search term.</p>\n";
+    htmlstring += '</div>\n';
+    //Creating an error message if no data is returned
+  }
 
   $('#results').html(htmlstring);
   //Injecting the HTML into the empty container
