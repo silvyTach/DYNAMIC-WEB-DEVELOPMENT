ca3ea13044f25033ba36e363238f48b2bbb93e38
hughtml <h.m.m.fraser@rgu.ac.uk>
2018-04-17 19:24:53 +0100

    Testing push.

--- Coursework/public/scripts/searchresults.js
+++ Coursework/public/scripts/searchresults.js
@@ -1,6 +1,6 @@
 $(function (){
 //Function will run when the page is loaded
-    alert("Document is ready.");
+    //alert("Document is ready.");
     //The document is ready
 
     $('#search').submit(function() {
