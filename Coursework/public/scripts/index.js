414946891c69269b4e273ddf9362186eeb751d55
AshayaPi <silviya_tacheva@outlook.com>
2018-04-18 11:15:56 +0100

    getting results from omdb

---
+++ Coursework/public/scripts/index.js
@@ -0,0 +1,27 @@
+$(function(){
+  //document ready
+  alert("Document Ready");
+  getResultsFromOMDB();
+  // $('#searchform').submit(function(){
+  //   var searchterms = $('#searchterms').val();
+  //   //addItemToList(searchterms);
+  //   getResultsFromOMDB(searchterms);
+  //   return false;
+  // });
+});
+function getResultsFromOMDB() {
+  var url = 'http://www.omdbapi.com/?apikey=280e6eab&type=movie';
+  $.getJSON(url, function(jsondata) {
+    addResultTitles(jsondata);
+  });
+
+  function addResultTitles(jsondata) {
+    var htmlstring = "";
+    for(var i=0; i<5; i++){
+      htmlstring+= jsondata.Search[i];
+    }
+    console.log(htmlstring);
+  }
+}
+
+// ------------------------------------------------------------------------------------------------------
