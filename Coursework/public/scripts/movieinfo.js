3a8570e2f57b71ad56ae710aa3cbeb6df4e4780f
AshayaPi <silviya_tacheva@outlook.com>
2018-04-19 12:30:02 +0100

    testing plot

---
+++ Coursework/public/scripts/movieinfo.js
@@ -0,0 +1,20 @@
+$(function(){
+  //document ready
+  alert($('#title').html);
+  omdb($('#title').html);
+  // Top 5 Comedies
+  });
+function omdb(title) {
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "hhttp://www.omdbapi.com/?t=" + title+ "&plot=full&apikey=280e6eab",
+    "method": "GET",
+    "headers": {},
+    "data": "{}"
+  }
+$.ajax(settings).done(function (response) {
+  $('#plot').html(response.Plot)
+});
+
+}
