d5e307b16f6340dc71cec18879a6c9221e20cf1e
AshayaPi <silviya_tacheva@outlook.com>
2018-04-21 23:18:02 +0100

    login

--- Coursework/server.js
+++ Coursework/server.js
@@ -99,21 +99,44 @@ app.post('/login', function(req, res) {
   var uname = req.body.loginformusername;
   var pword = req.body.loginformpassword;
 
-  db.collection('people').findOne({"login.username":uname}, function(err, result) {
+  db.collection('users').findOne({"login.username":uname}, function(err, result) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
-    if(!result){res.redirect('/login');return}
+    if(!result){res.redirect('/signuplogin');return}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
-    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
+    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/library') }
     //otherwise send them back to login
-    else{res.redirect('/login')}
+    else{res.redirect('/signuplogin')}
   });
 });
 
-
-
-
-
+// app.post('/login', function(req, res) {
+//   console.log(JSON.stringify(req.body));
+//   var uname = req.body.loginformusername;
+//   var pword = req.body.loginformpassword;
+//   Getting the username and password entered by the user
+//   db.collection('users').findOne({"username":uname}), function (error, result) {
+//     if (error) {
+//       throw error;
+//       If there's an error, throw it
+//     }
+//
+//     if (!result) {
+//       res.redirect('/login');
+//       return;
+//       //If there is no result matching the username, a new login page is generated
+//     }
+//
+//     if (result.password == pword) {
+//       res.redirect('/library');
+//       //The user is redirected to their library
+//     } else {
+//       res.redirect('/login');
+//       return;
+//       If there is no result matching the username, a new login page is generated
+//     }
+//   };
+// });
 
 app.post('/dosearch', function(req, res) {
   console.log(JSON.stringify(req.body));
