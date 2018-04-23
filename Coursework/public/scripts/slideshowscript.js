6fd21eb896a80a386742043a8dea9061d52b199a
AshayaPi <silviya_tacheva@outlook.com>
2018-04-23 07:15:54 +0100

    slideshow

--- Coursework/public/scripts/slideshowscript.js
+++ Coursework/public/scripts/slideshowscript.js
@@ -1,17 +1,21 @@
 var slideIndex = 1;
-showDivs(slideIndex);
+showSlides(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showDivs(n) {
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName("slideshow");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
+  var slides = document.getElementsByClassName("slideshow");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";
 }
