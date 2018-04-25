d7b0dea53209243b37c0ea120807e2f6be21e4e2
AshayaPi <silviya_tacheva@outlook.com>
2018-04-25 11:01:06 +0100

    ids

--- Coursework/public/scripts/navbar.js
+++ Coursework/public/scripts/navbar.js
@@ -14,33 +14,12 @@ $(function(){
     }
   }
   console.log("user: " + user);
-  
+
   if(user != "") change();
   });
   function change() {
-    var li = $('navbar li a');
-    li[1].html('My movies');
-    li[1].href("/user");
-    li[2].html('Sign out');
-    li[2].href("/");
-  }
-
-
-  //grab the url in the location bar
-  var searchString = document.location.search;
-
-  //strip off the heading '?'
-  searchString = searchString.substring(1);
-
-  //split string of arguments into name=value pairs
-  var nvPairs = searchString.split("&");
-
-  //iterate through name=value pairs
-  //split into separate name and value
-  //print to the browser console
-  for (var i = 0; i < nvPairs.length; i++) {
-    var nvPair = nvPairs[i].split("=");
-    var name = nvPair[0];
-    var value = nvPair[1];
-    console.log("argument name="+name+" and value="+value);
+    $('#menu2').html('My movies');
+    $('#menu2').href("/user");
+    $('#menu3').html('Sign out');
+    $('#menu3').href("/");
   }
