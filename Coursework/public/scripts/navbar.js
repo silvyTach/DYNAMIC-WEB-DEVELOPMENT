1261b1d18d5b07802b9c12a949ffdfaffbcac42a
AshayaPi <silviya_tacheva@outlook.com>
2018-04-25 11:31:35 +0100

    adding db

--- Coursework/public/scripts/navbar.js
+++ Coursework/public/scripts/navbar.js
@@ -1,4 +1,19 @@
 $(function(){
+  var db;
+  //Variable to store the database
+
+  MongoClient.connect(url, function(error, database) {
+    if (error) {
+      throw error;
+      //If there's an error, throw it
+    }
+    db = database;
+    //Storing the database in the variable
+    app.listen(8080);
+    //Telling the app to listen on port 8080
+    console.log('8080 is the magic port.');
+    //Displaying a message in the console
+  });
   //document ready
   //alert("Document Ready");
   var searchString = document.location.search;
