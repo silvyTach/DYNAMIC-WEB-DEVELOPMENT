3d0cad41a1c222c93272643884bb2e3d45dd6738
AshayaPi <silviya_tacheva@outlook.com>
2018-04-25 10:57:13 +0100

    user url

--- Coursework/public/scripts/navbar.js
+++ Coursework/public/scripts/navbar.js
@@ -1,7 +1,21 @@
 $(function(){
   //document ready
   //alert("Document Ready");
-  if(req.query.user != "") change();
+  var searchString = document.location.search;
+  searchString = searchString.substring(1);
+  var nvPairs = searchString.split("&");
+  var user;
+  for (var i = 0; i < nvPairs.length; i++) {
+    var nvPair = nvPairs[i].split("=");
+    var name = nvPair[0];
+    if(name == "user") {
+      user = nvPair[1];
+      break;
+    }
+  }
+  console.log("user: " + user);
+  
+  if(user != "") change();
   });
   function change() {
     var li = $('navbar li a');
@@ -9,4 +23,24 @@ $(function(){
     li[1].href("/user");
     li[2].html('Sign out');
     li[2].href("/");
+  }
+
+
+  //grab the url in the location bar
+  var searchString = document.location.search;
+
+  //strip off the heading '?'
+  searchString = searchString.substring(1);
+
+  //split string of arguments into name=value pairs
+  var nvPairs = searchString.split("&");
+
+  //iterate through name=value pairs
+  //split into separate name and value
+  //print to the browser console
+  for (var i = 0; i < nvPairs.length; i++) {
+    var nvPair = nvPairs[i].split("=");
+    var name = nvPair[0];
+    var value = nvPair[1];
+    console.log("argument name="+name+" and value="+value);
   }
