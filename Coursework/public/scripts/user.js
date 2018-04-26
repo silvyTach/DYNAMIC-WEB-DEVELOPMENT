e937d2e9d3536b23c090b6a819726ecb6da72289
AshayaPi <silviya_tacheva@outlook.com>
2018-04-26 23:18:48 +0100

    commenting and removing testing code

--- Coursework/public/scripts/user.js
+++ Coursework/public/scripts/user.js
@@ -1,44 +1,41 @@
-$(function(){
-  //document ready
-  //alert("Document Ready");
-  var user = $("#user").html();
-  var length = $("#length").html();
-  // var id = $('.movie').html();
-  console.log(length);
-  for (var i = 0; i < length; i++) {
-    var id= $("#movie"+i).html();
-    console.log(id);
-    getInfo(id, user);
-     $('#movie' + i).remove();
-  }
-   $('#length').remove();
-  // for (var i = 0; i < id.length; i++) {
-  //   getInfo(id[i]);
-  //   $('.id').remove();
-  // }
-  // getInfo(id);
-  });
+$(function() {
+    var user = $("#user").html();
+    //variable to save the user name so it can be passed via the url
+    var length = $("#length").html();
+    //variable to get number of movies in the user library
 
-
-  function getInfo(id, user) {
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://api.themoviedb.org/3/movie/" + id + "?api_key=305a3b42d88760bd22c9f8c8c54f788d",
-      "method": "GET",
-      "headers": {},
-      "data": "{}"
+    //for each movie the movie id is acquired from the p tag with a certain id
+    // the id is then used to fetch information from TMDB api
+    //the p tag with that movie id is removed
+    for (var i = 0; i < length; i++) {
+        var id = $("#movie" + i).html();
+        getInfo(id, user);
+        $('#movie' + i).remove();
     }
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      var html = "";
-        html+= '<div class="grid-20 mobile-grid-45 tablet-grid-45 grid-parent librarybox">'
-        html+= '<div class="grid-100 libraryimage"> <img class="poster" '
-        html+= "src='https://image.tmdb.org/t/p/w185" + response.poster_path + "' alt='" + response.title + " poster'></div>"
-        html+= '<div class="grid-100 librarytitle"><h4 class="title">' + response.title + "</h4></div>"
+    //the p tag with the number of movies in the user library is removed
+    $('#length').remove();
+});
 
-        html+= '<div class="grid-100 librarybuttons"><a href="/movieshowinfo?user='+ user + '&id=' + response.id + '" class="button">More Details</a>'
-        html+= '<button class="button">Remove</button></div></div>'
-      $('#library').append(html);
+//Function that gets info from the API
+function getInfo(id, user) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.themoviedb.org/3/movie/" + id + "?api_key=305a3b42d88760bd22c9f8c8c54f788d",
+        "method": "GET",
+        "headers": {},
+        "data": "{}"
+    }
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+        var html = "";
+        html += '<div class="grid-20 mobile-grid-45 tablet-grid-45 grid-parent librarybox">'
+        html += '<div class="grid-100 libraryimage"> <img class="poster" '
+        html += "src='https://image.tmdb.org/t/p/w185" + response.poster_path + "' alt='" + response.title + " poster'></div>"
+        html += '<div class="grid-100 librarytitle"><h4 class="title">' + response.title + "</h4></div>"
+
+        html += '<div class="grid-100 librarybuttons"><a href="/movieshowinfo?user=' + user + '&id=' + response.id + '" class="button">More Details</a>'
+        html += '<button class="button">Remove</button></div></div>'
+        $('#library').append(html);
     });
-  }
+}
