1e86ce133f7b2a869d1cb9e20eb22cec58577d44
AshayaPi <silviya_tacheva@outlook.com>
2018-04-26 13:17:08 +0100

    fixing js

--- Coursework/public/scripts/user.js
+++ Coursework/public/scripts/user.js
@@ -1,13 +1,14 @@
 $(function(){
   //document ready
   //alert("Document Ready");
-  var id = $('#id').html();
-  similarMovies(id);
-  director(id);
-  $('#id').remove();
+  var id = $('.movie').html();
+  for (var i = 0; i < id.length; i++) {
+    getInfo(id[i]);
+    $('.id').remove();
+  }
   });
 
-  function similarMovies(id) {
+  function getInfo(id) {
     var settings = {
       "async": true,
       "crossDomain": true,
@@ -20,30 +21,14 @@ $(function(){
       console.log(response);
       var html = "";
       for(var i=0; i<3; i++){
-        html+= '<tr><td><a href="/movieshowinfo?id=' + response.results[i].id + '">'
-        html+= "<img src='https://image.tmdb.org/t/p/w154" + response.results[i].poster_path + "' alt='" + response.results[i].title + " poster'></a></td><td>"
-        html+= "<h1>" + response.results[i].title + "</h1>"
+        html+= '<div class="grid-20 mobile-grid-45 tablet-grid-45 grid-parent librarybox">'
+        html+= 'div class="grid-100 libraryimage"> <img class="poster" '
+        html+= "src='https://image.tmdb.org/t/p/w185" + response.results[i].poster_path + "' alt='" + response.results[i].title + " poster'></div>"
+        html+= '<div class="grid-100 librarytitle"><h4 class="title">' + response.results[i].title + "</h4></div>"
 
-        html+= '<p class="l-items">⭐️ ' + response.results[i].vote_average + '<i> /10</i></p>'
-        html+= '<p class="l-items">Release Date: ' + response.results[i].release_date + ' </p>'
-        html+= '<p>' + response.results[i].overview + ' </p></td></tr>'
+        html+= '<div class="grid-100 librarybuttons"><button class="button">More Details</button>'
+        html+= '<button class="button">Remove</button></div></div>'
       }
-      $('#similar table').html(html);
+      $('#library').html(html);
     });
   }
-
-  function director(id) {
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api.themoviedb.org/3/movie/" + id + "/credits?api_key=305a3b42d88760bd22c9f8c8c54f788d",
-  "method": "GET",
-  "headers": {},
-  "data": "{}"
-}
-
-$.ajax(settings).done(function (response) {
-  console.log(response.crew[0].name);
-  $('#director').html(response.crew[0].name);
-});
-}
