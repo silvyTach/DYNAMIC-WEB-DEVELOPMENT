4523be6d931b8679773236f1011286f545cd8517
AshayaPi <silviya_tacheva@outlook.com>
2018-04-26 10:51:32 +0100

    add movie

--- Coursework/server.js
+++ Coursework/server.js
@@ -176,3 +176,13 @@ app.post('/signup', function(req, res) {
     })
   }
 });
+
+app.post('/addMovie', function(req, res) {
+  var query = { login.username: req.body.user };
+  var newvalues = { $push: {library: req.body.id} };
+  db.collection('quotes').updateOne(query,newvalues, function(err, result) {
+    if (err) throw err;
+    console.log("added movie" + req.body.id + " to " + req.body.user);
+    res.redirect('/');
+  });
+});
