dd709f545161eb14efb74937eee09fb096093503
AshayaPi <silviya_tacheva@outlook.com>
2018-04-27 13:30:43 +0100

    testing function

--- Coursework/public/scripts/library.js
+++ Coursework/public/scripts/library.js
@@ -6,44 +6,35 @@ $(function() {
     var id = $(p[i]).html();
     p[i].remove();
     var name = $("#b" + id).html().substring(1);
-    console.log(id + " : " + name);
-
+    // console.log(id + " : " + name);
+    genre(id, name);
   }
-  // $(".tab p").forEach(function(p){
-  //   var id = p.html();
-  //   var name = $("#g" + id).html();
-  //   console.log(id + " : " + name);
-  //   p.remove();
-  // })
-
-
-
     // Top 5 Comedies
     // top5(35, "Comedy");
 });
 
-// function top5(id, genre) {
-//     var settings = {
-//         "async": true,
-//         "crossDomain": true,
-//         "url": "https://api.themoviedb.org/3/discover/movie?with_genres=" + id + "&page=1&sort_by=popularity.desc&api_key=305a3b42d88760bd22c9f8c8c54f788d",
-//         "method": "GET",
-//         "headers": {},
-//         "data": "{}"
-//     }
-//     $.ajax(settings).done(function(response) {
-//         var html = '<div class="top"><h2>' + genre + ' <i>#Top5</i></h2><a href="/library">view all ></a></div>';
-//         for (var i = 0; i < 5; i++) {
-//             html += "<div class='poster-title'>"
-//             html += "<img src='https://image.tmdb.org/t/p/original/" + response.results[i].poster_path +
-//                 "' width = '261px' alt='" + response.results[i].title + " poster'>"
-//             html += "<span>" + response.results[i].title + "</span>"
-//             html += "<div class='imageInner'>"
-//             html += '<span><a href="/movieshowinfo?user=' + user + '&id=' + response.results[i].id + '" class="poster-button" tabindex="-1">more details</a></span>'
-//             html += "</div></div>"
-//                 console.log(response.results[i].title);
-//             $("#" + genre).html(html);
-//         }
-//     });
-//
-// }
+function genre(id, name) {
+  var settings = {
+          "async": true,
+          "crossDomain": true,
+          "url": "https://api.themoviedb.org/3/discover/movie?with_genres=" + id + "&page=1&sort_by=popularity.desc&api_key=305a3b42d88760bd22c9f8c8c54f788d",
+          "method": "GET",
+          "headers": {},
+          "data": "{}"
+      }
+    $.ajax(settings).done(function(response) {
+        var html = "";
+        for (var i = 0; i < response.results.length; i++) {
+            html += '<div class="grid-20 mobile-grid-45 tablet-grid-45 grid-parent librarybox">'
+            html += '<div class="grid-100 libraryimage">'
+            html += '<img class="poster" src="https://image.tmdb.org/t/p/original/' + response.results[i].poster_path + '" '
+            html += 'alt="Poster"></div>'
+            html += '<div class="grid-100 librarytitle">'
+            html += '<h4 class="title">' + response.results[i].title + '</h4></div>'
+            html += '<div class="grid-100 librarybuttons">'
+            html += '<button class="button">More Details</button>'
+            html += "</div></div>"
+            $("#" + id).html(html);
+        }
+    });
+}
