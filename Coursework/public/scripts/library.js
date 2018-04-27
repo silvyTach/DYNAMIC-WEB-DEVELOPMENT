3f7a87c69fed93ff4295ee58f848ef03676bf55f
AshayaPi <silviya_tacheva@outlook.com>
2018-04-27 14:19:32 +0100

    testing history

--- Coursework/public/scripts/library.js
+++ Coursework/public/scripts/library.js
@@ -12,17 +12,16 @@ $(function() {
 });
 
 function genre(id, name) {
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api.themoviedb.org/3/discover/movie?with_genres=" + id + "&page=1&sort_by=popularity.desc&api_key=305a3b42d88760bd22c9f8c8c54f788d",
-        "method": "GET",
-        "headers": {},
-        "data": "{}"
-    }
+  var settings = {
+          "async": true,
+          "crossDomain": true,
+          "url": "https://api.themoviedb.org/3/discover/movie?with_genres=" + id + "&page=1&sort_by=popularity.desc&api_key=305a3b42d88760bd22c9f8c8c54f788d",
+          "method": "GET",
+          "headers": {},
+          "data": "{}"
+      }
     $.ajax(settings).done(function(response) {
-      console.log(response.body);
-      var html = "";
+        var html = "";
         for (var i = 0; i < response.results.length; i++) {
             html += '<div class="grid-20 mobile-grid-45 tablet-grid-45 grid-parent librarybox">'
             html += '<div class="grid-100 libraryimage">'
@@ -33,8 +32,7 @@ function genre(id, name) {
             html += '<div class="grid-100 librarybuttons">'
             html += '<button class="button">More Details</button>'
             html += "</div></div>"
-            $("#" + id).append(html);
+            $("#" + id).html(html);
         }
-
     });
-  }
+}
