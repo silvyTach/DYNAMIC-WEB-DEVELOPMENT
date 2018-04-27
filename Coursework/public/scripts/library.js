239e55e51e8e1d1fa6d2530549a08738d37f117f
AshayaPi <silviya_tacheva@outlook.com>
2018-04-27 21:48:51 +0100

    library

--- Coursework/public/scripts/library.js
+++ Coursework/public/scripts/library.js
@@ -78,16 +78,3 @@ function popular(user) {
         });
     }
 }
-© 2018 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-API
-Training
-Shop
-Blog
-About
-Press h to open a hovercard with more details.
