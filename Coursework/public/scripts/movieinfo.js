8a8a184dabd6d5a07e5f198f1d249fd151a6a6ff
AshayaPi <silviya_tacheva@outlook.com>
2018-04-27 19:16:40 +0100

    require

--- Coursework/public/scripts/movieinfo.js
+++ Coursework/public/scripts/movieinfo.js
@@ -15,6 +15,20 @@ $(function(){
   $("#user").css("display", "none");
   $("#movie").css("display", "none");
 
+  function(require) {
+    var databaseUrl = "mongodb://localhost:27017/";
+    var collections = ["users"];
+    var db = require("mongojs").connect(databaseUrl, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("wheresmymovie");
+      var query = {"login.username":user};
+      dbo.collection("users").find(query).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result.library);
+        db.close();
+      });
+    })
+  }
 
 
   var id = $('#id').html();
