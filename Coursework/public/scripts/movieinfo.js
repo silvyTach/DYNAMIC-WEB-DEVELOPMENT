15a38e731200a96f4d59d2f95edf426510976dd4
AshayaPi <silviya_tacheva@outlook.com>
2018-04-27 06:28:40 +0100

    user info for the movieshow page

--- Coursework/public/scripts/movieinfo.js
+++ Coursework/public/scripts/movieinfo.js
@@ -1,6 +1,19 @@
 $(function(){
   //document ready
   //alert("Document Ready");
+  var searchString = document.location.search;
+  searchString = searchString.substring(1);
+  var nvPairs = searchString.split("&");
+  var user;
+  for (var i = 0; i < nvPairs.length; i++) {
+    var nvPair = nvPairs[i].split("=");
+    var name = nvPair[0];
+    if(name == "user") {
+      user = nvPair[1];
+      break;
+    }
+  }
+  $("#user").val(user);
   $("#user").css("display", "none");
   $("#movie").css("display", "none");
   var id = $('#id').html();
