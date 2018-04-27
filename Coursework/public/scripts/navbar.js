c2e3e74f581ce59137c6be549b1cab139550f58f
AshayaPi <silviya_tacheva@outlook.com>
2018-04-27 06:09:15 +0100

    adding username to the navbar links

--- Coursework/public/scripts/navbar.js
+++ Coursework/public/scripts/navbar.js
@@ -3,17 +3,27 @@ $(function(){
   //alert("Document Ready");
   var searchString = document.location.search;
   searchString = searchString.substring(1);
-  //Checks if there is userinfo in the url
-
+  var nvPairs = searchString.split("&");
+  var user;
+  for (var i = 0; i < nvPairs.length; i++) {
+    var nvPair = nvPairs[i].split("=");
+    var name = nvPair[0];
+    if(name == "user") {
+      user = nvPair[1];
+      break;
+    }
+  }
   // console.log("searchString " + searchString);
-  
+
+  // Checks if there is userinfo in the url
   // if thre is, the content of the navigation bar changes
-  if(searchString != "") change();
+  if(searchString != "") change(user);
   });
 
   function change() {
+    $('#menu1').attr("href", "/library?user=" + user);
     $('#menu2').html('My movies');
-    $('#menu2').attr("href", "/user");
+    $('#menu2').attr("href", "/user?user=" + user);
     $('#menu3').html('Sign out');
     $('#menu3').attr("href", "/");
   }
